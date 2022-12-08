4056561a3e8db7e00d60ce9d85eb545cda43c005
felipe <felipegraca.1999@gmail.com>
2022-12-08 02:43:49 -0300

    feature: Create SpaceMediaModel test

--- test/features/data/repositories/space_media_repository_test.dart
+++ test/features/data/repositories/space_media_repository_test.dart
@@ -4,8 +4,9 @@ import 'package:mocktail/mocktail.dart';
 import 'package:nasa_clean_arch/core/errors/exceptions.dart';
 import 'package:nasa_clean_arch/core/errors/failures.dart';
 import 'package:nasa_clean_arch/features/data/datasources/space_media_datasource.dart';
-import 'package:nasa_clean_arch/features/data/models/space_media_model.dart';
 import 'package:nasa_clean_arch/features/data/repositories/space_media_repository.dart';
+
+import '../../../mocks/space_media_mock.dart';
 
 class MockeSpaceMediaDatasource extends Mock implements ISpaceMediaDatasouce {}
 
@@ -18,23 +19,15 @@ void main() {
     repository = SpaceMediaRepository(datasouce);
   });
 
-  const tSpaceMediaModel = SpaceMediaModel(
-    description:
-        '''Mars looks sharp in these two rooftop telescope views captured in late November from Singapore, planet Earth. At the time, Mars was about 82 million kilometers from Singapore and approaching its opposition, opposite the Sun in planet Earth's sky on December 8. Olympus Mons, largest of the volcanoes in the Tharsis Montes region (and largest known volcano in the Solar System), is near Mars' western limb. In both the images it's the whitish donut-shape at the upper right. The dark area visible near center is the Terra Sirenum region while the long dark peninsula closest to the planet's eastern limb is Sinus Gomer. Near its tip is Gale crater, the Curiosity rover's landing site in 2012. Above Sinus Gomer, white spots are other volcanoes in the Elysium region. At top of the planet is the north polar cap covered with ice and clouds. Taken about two days apart, these images of the same martian hemisphere form a stereo pair. Look at the center of the frame and cross your eyes until the separate images come together to see the Red Planet in 3D.''',
-    mediaType: 'image',
-    title: 'Stereo Mars near Opposition',
-    mediaUrl: 'https://apod.nasa.gov/apod/image/2212/Mars-Stereo.png',
-  );
-
   final tDate = DateTime(2021, 12, 1);
 
   test('should return space media model when calls the datasource', () async {
     // arrange
-    when(() => datasouce.getSpaceMediaFromDate(any())).thenAnswer((_) async => tSpaceMediaModel);
+    when(() => datasouce.getSpaceMediaFromDate(any())).thenAnswer((_) async => tSpaceMedia);
     // act
     final result = await repository.getSpaceMediaFromDate(tDate);
     // assert
-    expect(result, const Right(tSpaceMediaModel));
+    expect(result, const Right(tSpaceMedia));
     verify(() => datasouce.getSpaceMediaFromDate(tDate)).called(1);
   });
 
